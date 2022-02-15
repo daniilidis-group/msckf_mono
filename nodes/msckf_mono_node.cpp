ec94c2dd0519ea46702ea1c79eb8e3f11ea40463
removedporn <86824510+removedporn@users.noreply.github.com>
2022-02-15 19:39:38 +0800

    Delete nodes directory

--- nodes/msckf_mono_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <ros/ros.h>
-#include <msckf_mono/ros_interface.h>
-
-int main(int argc, char** argv) {
-  ros::init(argc, argv, "msckf_mono_node");
-  ros::NodeHandle nh;
-  msckf_mono::RosInterface ri(nh);
-  ros::spin();
-}
